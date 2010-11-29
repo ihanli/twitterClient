20eb4827176884fea0913087a3bee0003845108a
Ismail Hanli <ihanli.mmt-b2009@fh-salzburg.ac.at>
2010-11-29 10:01:37 +0100

    renamed two files

--- main.cpp
+++ main.cpp
@@ -11,16 +11,8 @@ int main(int argc, char **argv)
 	char kbInput[140];
 	char message[140];
 
-// if(argc == 1)
-// {
-//	printf("Please enter an IP and port (port is optional).\n");
-//	getline(cin, input);
-//	scanf("%s", *argv);
-// }
-
 	printf("Please enter an IP and port (port is optional).\n");
 	getline(cin, input);
-//	scanf("%s", kbInput);
 
     Client myClient(input.c_str());
 
@@ -36,14 +28,6 @@ int main(int argc, char **argv)
     while(1){
         printf("u sayin': ");
         getline(cin, input);
-//        twitterCommand << input;
-//        twitterCommand >> *kbInput;
-//        scanf("%s", kbInput);
-
-//        if(kbInput[0] == 'q'){
-//            myClient.sendToServer(input.c_str());
-//            break;
-//        }
 
         try{
             myClient.sendToServer(input.c_str());
